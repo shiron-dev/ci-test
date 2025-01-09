716296e663cc02f977822855cfea3c21f70c83d0
shiron <shiron4710.dev@gmail.com>
2025-01-09 11:12:03 +0900

    fix shellcheck

--- scripts/renovate_dryrun.sh
+++ scripts/renovate_dryrun.sh
@@ -6,6 +6,6 @@ renovate-config-validator --strict
 
 export RENOVATE_CONFIG_FILE=.github/renovate.json
 
-export RENOVATE_TOKEN="${RENOVATE_TOKEN:-$(read -p "環境変数 'RENOVATE_TOKEN' が設定されていません。値を入力してください: " input_value; echo "$input_value")}"
+export RENOVATE_TOKEN="${RENOVATE_TOKEN:-$(read -p -r "環境変数 'RENOVATE_TOKEN' が設定されていません。値を入力してください: " input_value; echo "$input_value")}"
 
 LOG_LEVEL=debug renovate shiron-dev/images --dry-run=full --require-config=ignored --schedule=""
